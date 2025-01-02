dce13510e49b1f867c0efd9c386d4bc5b59e7185
ryan <ryanliu12620@gmail.com>
2025-01-02 10:16:47 -0500

    Changed diff file

--- Train.java
+++ Train.java
@@ -2,9 +2,12 @@ public class Train {
     Boolean onFire;
     public Train (Boolean onFire) {
         this.onFire = onFire;
+        onFire = false;
     }
 
     public Boolean getOnFire() {
         return onFire;
     }
+
+
 }
